e71d72572732aa70f478d75de29ab20d7118ba1f
HiddenStoweAway <cashoto13@gmail.com>
2026-05-20 18:51:43 -0700

    Added Top Page Bar with Login and other test pages

---
+++ lib/pages/p_how_it_works.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HowItWorks extends StatefulWidget {
+  const HowItWorks({super.key});
+
+  @override
+  State<HowItWorks> createState() => _HowItWorksState();
+}
+
+class _HowItWorksState extends State<HowItWorks> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
