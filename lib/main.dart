3b34a8ce76e6a4adf5009e0115ad980de39c91a5
HiddenStoweAway <cashoto13@gmail.com>
2026-06-17 18:15:23 -0400

    Added full page so teacher  can view student progress

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
 
       theme: ThemeData(
-        colorScheme: .fromSeed(
+        colorScheme: ColorScheme.fromSeed(
           seedColor: const Color.fromARGB(255, 126, 167, 255),
         ),
       ),
