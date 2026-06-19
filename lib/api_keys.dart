ce2a18713999abb35ed3438d9a4f1bcd6d2db18a
HiddenStoweAway <cashoto13@gmail.com>
2026-06-19 18:56:45 -0400

    Getting ready for build actually this time

---
+++ lib/api_keys.dart
@@ -0,0 +1,6 @@
+class ApiKeys {
+  static ApiKeys instance = ApiKeys();
+
+  final supabaseUrl = 'https://gtybiqtjcgslvoiovhuy.supabase.co';
+  final anonKey = 'sb_publishable_Bvg2YeRaNMEwArDD2lJggw_f61Qn4N0';
+}
